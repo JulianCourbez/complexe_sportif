5b9e175e2a843f70a3802b8f8bdd7c4a4640ce8b
JulianCourbez <julian.courbez@gmail.com>
2022-05-04 16:28:19 +0200

    Initial commit

---
+++ src/main/java/com/complexe/servlets/HelloServlet.java
@@ -0,0 +1,35 @@
+package com.complexe.servlets;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+
+    public HelloServlet()
+    {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        EntityManagerFactory emf  = Persistence.createEntityManagerFactory("complexe_PU");
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+        response.getWriter().append("Connexion: ").append(emf.toString());
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        doGet(request, response);
+    }
+}
+
